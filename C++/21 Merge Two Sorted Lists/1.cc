edee43f2dc0f268efef1a3380bab1d63892ae243
thaihoangdang <thaihoangdang1803@gmail.com>
2023-09-19 23:42:00 +0700

    704 binary search

---
+++ C++/21 Merge Two Sorted Lists/1.cc
@@ -0,0 +1,22 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// runtime: O(n)
+// space: O(n)
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+    }
+};
