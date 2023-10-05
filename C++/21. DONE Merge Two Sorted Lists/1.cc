665ad0964db5503f140de411f75bea59c7c7d738
thaihoangdang <thaihoangdang1803@gmail.com>
2023-10-05 22:13:49 +0700

    minor

--- C++/21. DONE Merge Two Sorted Lists/1.cc
+++ C++/21. DONE Merge Two Sorted Lists/1.cc
@@ -20,16 +20,18 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode *head = nullptr, *curNode = nullptr;
 
-        if (list1 && (! list2)) return list1;
-        if (list2 && (! list1)) return list2;
-        if ((! list2) && (! list1)) return head;
+        if (list1 && (! list2)) return list1; // return list1 if list2 is nullptr
+        if (list2 && (! list1)) return list2; // return list2 if list1 is nullptr
+        if ((! list2) && (! list1)) return head; // return nullptr if two lists are nullptr
 
+        // update the first element of the list
         if (list1->val < list2->val) {
             head = list1; curNode = list1; list1 = list1->next;
         } else {
             head = list2; curNode = list2; list2 = list2->next;
         }
 
+        // traverse the two lists and update the combined list
         while (list1 && list2) {
             if (list1->val < list2->val) {
                 curNode->next = list1;
@@ -42,10 +44,9 @@ public:
             }
         }
 
+        // add the remaining list
         if (list1) curNode->next = list1;
         else if (list2) curNode->next = list2;
-
-        while (curNode->next) curNode = curNode->next;
 
         return head;
     }
