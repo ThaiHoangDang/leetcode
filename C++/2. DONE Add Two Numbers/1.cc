1aa77a8ed223993edaf5c48ab62b9dd0fe1d2025
thaihoangdang <thaihoangdang1803@gmail.com>
2023-10-11 11:54:51 +0700

    2. Add Two Numbers

--- C++/2. DONE Add Two Numbers/1.cc
+++ C++/2. DONE Add Two Numbers/1.cc
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-// runtime: O()
-// space: O()
+// runtime: O(n)
+// space: O(1)
 
 struct ListNode {
     int val;
@@ -18,20 +18,28 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int remain = 0, sum;
-        ListNode *returnedList = l1;
+        ListNode *returnedList = l1, *previous;
 
-        while (l1) {
-            sum = l1->val + l2->val + remain;
+        while (l1 || l2) {
+            if (l1 && l2) {
+                sum = l1->val + l2->val + remain; l2 = l2->next;
+            } else if (l1) {
+                sum = l1->val + remain;
+            } else if (l2) {
+                swap(l1, l2); previous->next = l1; sum = l1->val + remain;
+            }
 
             if (sum < 10) {
-                l1->val = sum;
+                l1->val = sum; remain = 0;
             } else if (sum < 20) {
                 l1->val = sum % 10; remain = 1;
             } else {
                 l1->val = sum % 20; remain = 2;
             }
-            l1 = l1->next; l2 = l2->next;
+            previous = l1; l1 = l1->next; 
         }
+
+        if (remain == 1) previous->next = new ListNode(1);
 
         return returnedList;
     }
