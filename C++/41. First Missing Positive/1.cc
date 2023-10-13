d1f8577ae06fa83bd1c38750a9b6aabd245afae6
thaihoangdang <thaihoangdang1803@gmail.com>
2023-10-13 12:54:05 +0700

    41. First Missing Positive

--- C++/41. First Missing Positive/1.cc
+++ C++/41. First Missing Positive/1.cc
@@ -1,21 +1,38 @@
 #include <vector>
 using namespace std;
 
-// time:
-// space:
+// time: O(n)
+// space: O(1)
 
 
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int left = 1, right, max;
+        int result = nums.size();
 
-        for (auto num : nums) if (num > max) max = num; 
+        if (nums.size() == 1 && nums[0] == 1) return 2;
         
-        right = max;
-
-        for (auto num : nums) {
-            if (num > left && num < right) left = num;
+        for (int i = 0; i < nums.size(); ) {
+            
+            if (nums[i] == i) { // num at correct position already
+                i++;
+            } else if (nums[i] >= 0 && nums[i] < nums.size()) { // num needs to be moved
+                if (nums[nums[i]] == nums[i]) nums[nums[i]] = -1; // avoid duplicates
+                swap(nums[i], nums[nums[i]]);
+            } else if (nums[i] == result) { // 
+                result++; 
+            } else { // negagive or out of range
+                nums[i] = -1; i++;
+            }
         }
+
+        // find the smallest
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return result;
     }
 };
