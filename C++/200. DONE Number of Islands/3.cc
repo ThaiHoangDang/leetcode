1681ae0f737177096fb878874d81cc7939db426e
thaihoangdang <thaihoangdang1803@gmail.com>
2023-10-16 22:09:55 +0700

    200. Number of Islands

--- C++/200. DONE Number of Islands/3.cc
+++ C++/200. DONE Number of Islands/3.cc
@@ -2,37 +2,34 @@
 using namespace std;
 
 // approach: DFS
-// time:
-// space:
+// time: O(n)
+// space: O(n)
 
-
-void expand(int x, int y, vector<vector<char>> &grid) {
-    vector<vector<int>> stack {vector<int>{x, y}};
-    int tempX, tempY;
-
-    while (! stack.empty()) {
-        tempX = stack.back()[0]; tempY = stack.back()[1];
-        stack.pop_back(); grid[tempY][tempX] = '2';
-
-        if (tempY - 1 >= 0 && grid[tempY - 1][tempX] == '1') stack.emplace_back(vector<int> {tempX, tempY - 1});
-
-        if (tempY + 1 < grid.size() && grid[tempY + 1][tempX] == '1') stack.emplace_back(vector<int> {tempX, tempY + 1});
-
-        if (tempX - 1 >= 0 && grid[tempY][tempX - 1] == '1') stack.emplace_back(vector<int> {tempX - 1, tempY});
-
-        if (tempX + 1 < grid[0].size() && grid[tempY][tempX + 1] == '1') stack.emplace_back(vector<int> {tempX + 1, tempY});
-    }
-}
 
 class Solution {
 
     public:
+        void expand(int x, int y, vector<vector<char>> &grid) {
+
+            // check if the coordinates are valid
+            if (y >= 0 && y < grid.size() && x >= 0 && x < grid[0].size() && grid[y][x] == '1') {
+
+                grid[y][x] = '2'; // marking as visited
+
+                expand(x, y - 1, grid); // up
+                expand(x, y + 1, grid); // down
+                expand(x - 1, y, grid); // left
+                expand(x + 1, y, grid); // right
+            }
+        }
+
         int numIslands(vector<vector<char>>& grid) {
             int numIslands = 0;
             int row = grid.size(), col = grid[0].size();
 
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
+                    // perform BFS
                     if (grid[i][j] == '1') { expand(j, i, grid); numIslands++; }
                 }
             }
