d130447c5217eb185d5e9a443c889ce9fa0a4eef
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-10 14:30:13 +0700

    155. Min Stack

---
+++ C++/Done/155. Min Stack/1.cc
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class MinStack {
+    int min = 0;
+    vector<int> stack; // a stack pushed values
+    vector<int> mins; // a stack of min-so-far after every push
+
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        stack.emplace_back(val);
+
+        if (mins.size() == 0 || val < mins.back()) mins.emplace_back(val);
+        else mins.emplace_back(mins.back());
+    }
+    
+    void pop() {
+        stack.pop_back();
+        mins.pop_back();
+    }
+    
+    int top() {
+        return stack.back();
+    }
+    
+    int getMin() {
+        return mins.back();
+    }
+};
